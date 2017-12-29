e4f864f79af926e4dd0feead71f6a72df155bdbf
LIUFEI <2239548447@qq.com>
2017-12-29 19:54:26 +0800

    11

--- CircuitService/src/main/java/com/znsd/circuit/service/UserService.java
+++ CircuitService/src/main/java/com/znsd/circuit/service/UserService.java
@@ -3,6 +3,15 @@ package com.znsd.circuit.service;
 import com.znsd.circuit.model.User;
 
 public interface UserService {
-
-	User user = null;
+	/**
+	 * 修改个人资料
+	 * @param user
+	 */
+	public void userUpdate(User user);
+	
+	/**
+	 * 修改密码
+	 * @param user
+	 */
+	public void updatePassword(User user);
 }
