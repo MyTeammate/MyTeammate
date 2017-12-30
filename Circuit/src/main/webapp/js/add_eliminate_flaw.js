e3eda320afa45e27285a0bf2000e29666cfc47e7
Administrator <Administrator@USER-20171222YW>
2017-12-30 15:08:17 +0800

    新增制定缺陷任务中的几个小功能

--- Circuit/src/main/webapp/js/add_eliminate_flaw.js
+++ Circuit/src/main/webapp/js/add_eliminate_flaw.js
@@ -1,7 +1,7 @@
-$(function(){
+$(function() {
 	$("#add_flaw").datagrid({
-		url : "eliminate/getAll",
-		height : 370,
+		url : "",
+		height : 380,
 		width : 865,
 		pagination : true,
 		pageNumber : 1,
@@ -9,40 +9,192 @@ $(function(){
 		rownumbers : true,
 		pagination : true,
 		pageList : [ 5, 10, 15, 20 ],
-		columns : [ [ 
-			{field : 'id',title : '自动编号',width : 82,checkbox : true},
-			{field : 'taskcoding',title : '任务编号',width : 82,align : 'center'},
-			{field : 'taskName',title : '任务名称',width : 82,align : 'center'},
-			{field : 'workBills',title : '工作单据',width : 82,align : 'center'},
-			{field : 'xiafaMan',title : '下发人',width : 92,align : 'center'},
-			{field : 'xiafaDate',title : '下发时间',width : 82,align : 'center'},
-			{field : 'taskStatus',title : '任务状态',width : 82,align : 'center'},
-            {field : 'wancdate',title : '任务完成时间',width : 82,align : 'center'},
-            {field : 'flag',title : '任务是否取消',width : 82,align : 'center'},
-            {field : 'operation',title : '操作',width : 136,align : 'center'}
-            ]],
-            toolbar:"#smalldiv"
+		columns : [ [{field : 'id',title : '自动编号',width : 82,checkbox : true},
+			{field : 'taskcoding',title : '线路编号',width : 82,align : 'center'},
+			{field : 'taskName',title : '杆塔编号',width : 82,align : 'center'},
+			{field : 'workBills',title : '缺陷等级',width : 82,align : 'center'},
+			{field : 'xiafaMan',title : '缺陷类型',width : 92,align : 'center'},
+			{field : 'xiafaDate',title : '缺陷描述',width : 82,align : 'center'},
+			{field : 'taskStatus',title : '发现人',width : 82,align : 'center'},
+            {field : 'wancdate',title : '发现时间',width : 82,align : 'center'},
+            {field : 'flag',title : '操作',width : 82,align : 'center',
+             formatter:function(value,row,index){
+				var oper='<span><a style="color:#008B00" href="javaScript:lookInspection('+row.id+')">移除</a>｜'
+				return  oper;
+			 }}
+            ] ],
+		toolbar : "#smalldiv"
 	});
+	$('#taskMan').combobox({
+		url : 'eliminate/getLineUser',
+		valueField : 'id',
+		textField : 'name',
+		width : 154,
+	});
+
+	$('#taskbills').combobox({
+		url : 'eliminate/getBills',
+		valueField : 'id',
+		textField : 'settingName',
+		width : 154,
+	});
+	
+	/*var t = $("#taskbills").val();
+	$.ajax({
+		url : "eliminate/getBills",
+		type : "post",
+		success : function(data) {
+			$("#taskbills").html("");
+			var str = "";
+			if (data) {
+
+				for (var i = 0; i < data.length; i++) {
+					str += "<option value='" + data[i].settingName
+							+ "' name='options'>" + data[i].settingName
+							+ "</option>"
+					$('#taskbills').combobox('setValues', data[i].settingName);
+				}
+			}
+			$("#taskbills").append(str);
+		}
+	})*/
+	
 });
-function flaw_add_staff(){
+$(function(){
+	/*$("#data_list").datalist({
+		url:"eliminate/getEliminateUser",
+		checkbox: true, 
+	    lines: true,
+	    
+	})*/
+		 $.ajax({
+				url : "eliminate/getEliminateUser",
+				type : "post",
+				success : function(data) {
+					$("#fb_list").html("");
+					var str = "";
+					if (data) {
+						for (var i = 0; i < data.length; i++) {
+							str += "<option value='" + data[i].id
+									+ "' name='options'>" + data[i].name
+									+ "</option>"
+						}
+					}
+					console.log(str);
+					$("#fb_list").append(str);
+				}
+			});
+		 $("#add").click(function(){
+			  if($("#fb_list option:selected").length>0) {
+			   $("#fb_list option:selected").each(function(){
+			    $("#select_list").append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option");
+			    
+			    $(this).remove();
+			   })
+			  }else {
+			   alert("请选择要添加的数据！");
+			  }
+			 });
+			 
+			 $("#delete").click(function() {
+			   if($("#select_list option:selected").length>0) {
+			    $("#select_list option:selected").each(function(){
+			     $("#fb_list").append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option");
+			      $(this).remove();
+			    })
+			   }else {
+			    alert("请选择要删除的数据！");
+			   }
+			 });
+})
+function save_user(){
+	 var str = $.map($("#select_list option:not(:selected)"),
+             function(ele){return ele.value} 
+          ).join(",");
+	 console.log(str);
+	 $.ajax({
+			url : "eliminate/getEliminateUserById?str="+str,
+			type : "post",
+			success : function(data) {
+				$("#select_user").html("");
+				var str = "";
+				if (data) {
+					console.log(data);
+					for (var i = 0; i < data.length; i++) {
+						str += "<option value='" + data[i].id
+								+ "' name='options'>" + data[i].name
+								+ "</option>"
+					}
+				}
+				
+				$("#select_user").append(str);
+			}
+		});
+	 $("#bigdiv").hide(1000);
+}
+function flaw_add_staff() {
 	alert(1);
 }
-function hid(){
+function hid() {
 	$("#bigdiv").hide(1000);
 }
-function show(){
+function show() {
 	$("#bigdiv").show(1000);
+	// $("#bigdiv").attr("style","display:block");
 }
-function returnto(){
-	move("制定消缺任务制定与分配","http://localhost:8080/Circuit/eliminate/eliminateflaw");
+function returnto() {
+	move("制定消缺任务制定与分配", "http://localhost:8080/Circuit/eliminate/eliminateflaw");
 }
-$(function(){
-	var t = $("#taskbills").val();
-	$.ajax({
-		url:"",
-		type:"post",
-		success:function(data){
-			var str="";
-		}
-	})
-})
+/*
+ * var t = $("#taskbills").val(); $.ajax({ url:"eliminate/getBills",
+ * type:"post", success:function(data){
+ * 
+ * $("#taskbills").html(""); var str=""; if(data){
+ * 
+ * for (var i = 0; i < data.length; i++) { str += "<option value='" +
+ * data[i].settingName + "' name='options'>" + data[i].settingName + "</option>"
+ * $('#taskbills').combobox('setValues',data[i].settingName); } }
+ * $("#taskbills").append(str); } })
+ */
+function add_eliminate(){
+	$("#xun_div").show(1000);
+	$("#xun_manager").datagrid({
+		url : "",
+		height : 270,
+		width : 500,
+		pagination : true,
+		pageNumber : 1,
+		pageSize : 5,
+		rownumbers : true,
+		pagination : true,
+		pageList : [ 5, 10, 15, 20 ],
+		columns : [ [{field : 'id',title : '自动编号',width : 32,checkbox : true},
+			{field : 'taskcoding',title : '线路编号',width : 62,align : 'center'},
+			{field : 'taskName',title : '杆塔编号',width : 62,align : 'center'},
+			{field : 'workBills',title : '缺陷等级',width : 62,align : 'center'},
+			{field : 'xiafaMan',title : '缺陷类型',width : 62,align : 'center'},
+			{field : 'xiafaDate',title : '缺陷描述',width : 62,align : 'center'},
+			{field : 'taskStatus',title : '发现人',width : 62,align : 'center'},
+            {field : 'wancdate',title : '发现时间',width : 62,align : 'center'},
+            ] ],	
+	});
+	
+}
+
+function xun_hid(){
+	$("#xun_div").hide(1000);
+}
+
+function taskSave() {
+	var taskcoding = $("#taskcoding").val();
+	var taskname = $("#taskname").val();
+	var taskbills = $("#taskbills").combobox('getText');
+	var taskMan = $("#taskMan").combobox('getValue');
+	var taskDesc = $("#taskDesc").val();
+	var taskRemark = $("#taskRemark").val();
+	var str = $.map($("#select_user option:not(:selected)"),
+            function(ele){return ele.value} 
+         ).join(",");
+	console.log(taskcoding, taskname, taskbills, taskMan, taskDesc, taskRemark,str);
+
+}
--- CircuitDao/src/main/java/com/znsd/circuit/dao/EliminateDao.java
+++ CircuitDao/src/main/java/com/znsd/circuit/dao/EliminateDao.java
@@ -1,10 +1,13 @@
 package com.znsd.circuit.dao;
+import java.security.PublicKey;
 import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.annotation.MapperScan;
 
 import com.znsd.circuit.model.Eliminate;
+import com.znsd.circuit.model.Systemparam;
+import com.znsd.circuit.model.User;
 
 @MapperScan
 public interface EliminateDao {
@@ -19,4 +22,36 @@ public interface EliminateDao {
 	 * @return
 	 */
 	public int getCount();
+	
+	/**
+	 * 查询出消缺任务的单据
+	 */
+	public List<Systemparam> getAllBills(String bills);
+	
+	/**
+	 * 查询出所有的线路管理员
+	 * @return
+	 */
+	public List<User> getAllLineUser();
+	
+	/**
+	 * 查询出所有的消缺员
+	 * @return
+	 */
+	public List<User> getAlleliminateUser();
+	
+	/**
+	 * 通过id查到消缺员
+	 */
+	public User getAllUserEliminate(int id);
+	
+	/**
+	 * 通过id修改任务状态
+	 */
+	public int updateTaskstatus(int id);
+	
+	/**
+	 * 查询出缺陷确认表中的数据
+	 */
+	//public List<Flaw>
 }
